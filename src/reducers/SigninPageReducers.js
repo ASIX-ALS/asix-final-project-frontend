77293fc81f61f54459843d83062edda74f47a8be
Leandro Gartner <gartnerleandro@gmail.com>
2017-05-25 20:19:54 +0200

    fix: resolved errors

--- src/reducers/SigninPageReducers.js
+++ src/reducers/SigninPageReducers.js
@@ -1,33 +1,27 @@
-"use strict"
-
-export function SigninPageReducers(state={user:'', pass:''}, action){
-  switch(action.type){
-    case "SIGNIN_SUCCESS":
-      return {
-        ...state,
-        user: action.user,
-        pass: action.pass,
-      };
-      break;
-    case "SIGNIN_FAILED":
-      return {
-        ...state,
-        user: '',
-        pass: '',
-      };
-      break;
-      case "CHANGE_USER":
-      return {
-        ...state,
-        user: action.user,
-      };
-      break;
-    case "CHANGE_PASS":
-      return {
-        ...state,
-        pass: action.pass,
-      };
-      break;
+export function SigninPageReducers(state = { user:'', pass:'' }, action) {
+  switch (action.type) {
+  case 'SIGNIN_SUCCESS':
+    return {
+      ...state,
+      user: action.user,
+      pass: action.pass,
+    };
+  case 'SIGNIN_FAILED':
+    return {
+      ...state,
+      user: '',
+      pass: '',
+    };
+  case 'CHANGE_USER':
+    return {
+      ...state,
+      user: action.user,
+    };
+  case 'CHANGE_PASS':
+    return {
+      ...state,
+      pass: action.pass,
+    };
+  default: return state;
   }
-  return state
 }
